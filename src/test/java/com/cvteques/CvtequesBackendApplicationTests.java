677288cc1d4b0464b475ac104126ab96bd8f2d78
Djibril <najidjibril@gmail.com>
2025-07-07 14:07:44 +0200

    first commit

---
+++ src/test/java/com/cvteques/CvtequesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvteques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvtequesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
